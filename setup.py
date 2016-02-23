a9b43a9244de4012187ccd2c7b8674561ed4e771
Marko Fritz <marko.fritz@embl.de>
2016-02-23 13:05:30 +0100

    PYPI

--- setup.py
+++ setup.py
@@ -17,28 +17,29 @@ if sys.version_info < (2, 7):
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='htseq-clip',
     version='0.1.0',
-    description='htseq-clip',
-    long_description=long_description,
+    description='htseqCLIP - a pipeline for the analysis of iCLIP datasets',
+	long_description=long_description,
     url='https://bitbucket.org/htseq-clip/htseq-clip',
     author='Marko Fritz',
     author_email='marko.fritz@embl.de',
-    license='EMBL',
+	zip_safe=False,
+    license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'License :: OSI Approved :: EMBL License',
+        'License :: OSI Approved :: MIT License',
         'Environment :: Console',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
     ],
-    packages=['htseq-clip', 'htseq-clip.egg-info', 'htseq-clip/lib'],
+    packages=['htseq-clip', 'htseq-clip/lib'],
     install_requires=['biopython', 'bokeh', 'HTSeq'],
     entry_points={
         'console_scripts': [
