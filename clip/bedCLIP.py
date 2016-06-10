7afc2aff2f38c5f62d9f94e0552aba15b562d796
nadiaashraf <nadia.ashraf98@gmail.com>
2016-06-10 18:04:41 +0000

    modified column labels in output file

--- clip/bedCLIP.py
+++ clip/bedCLIP.py
@@ -2,6 +2,7 @@
 # bedCLIP class
 # Authors: Marko Fritz, marko.fritz@embl.de
 #          Thomas Schwarzl, schwarzl@embl.de
+#          Nadia Ashraf
 # Institution: EMBL Heidelberg
 # Date: October 2015
 # --------------------------------------------------
@@ -47,7 +48,7 @@ class bedCLIP:
         d = {}
         
         for almnt in almnt_file:
-            
+
             if almnt.iv.strand == '+':
                 if not d.has_key(almnt.iv.chrom):
                     d[almnt.iv.chrom] = {almnt.iv.strand : [[almnt.iv.start_d, almnt.iv.end_d, almnt.name, int(almnt.score)]]}
@@ -55,7 +56,7 @@ class bedCLIP:
                     if not d[almnt.iv.chrom].has_key(almnt.iv.strand):
                         d[almnt.iv.chrom][almnt.iv.strand] = [[almnt.iv.start_d, almnt.iv.end_d, almnt.name, int(almnt.score)]]
                     else:
-                        d[almnt.iv.chrom][almnt.iv.strand].append([almnt.iv.start_d, almnt.iv.end_d, almnt.name, int(almnt.score)]) 
+                        d[almnt.iv.chrom][almnt.iv.strand].append([almnt.iv.start_d, almnt.iv.end_d, almnt.name, int(almnt.score)])
             else:
                 if not d.has_key(almnt.iv.chrom):
                     d[almnt.iv.chrom] = {almnt.iv.strand : [[almnt.iv.end_d, almnt.iv.start_d, almnt.name, int(almnt.score)]]}
@@ -63,7 +64,7 @@ class bedCLIP:
                     if not d[almnt.iv.chrom].has_key(almnt.iv.strand):
                         d[almnt.iv.chrom][almnt.iv.strand] = [[almnt.iv.end_d, almnt.iv.start_d, almnt.name, int(almnt.score)]]
                     else:
-                        d[almnt.iv.chrom][almnt.iv.strand].append([almnt.iv.end_d, almnt.iv.start_d, almnt.name, int(almnt.score)])        
+                        d[almnt.iv.chrom][almnt.iv.strand].append([almnt.iv.end_d, almnt.iv.start_d, almnt.name, int(almnt.score)])
                         
         return d       
     #===================================================================================
@@ -78,7 +79,10 @@ class bedCLIP:
         if self.fOutput.endswith(".gz"):
             output = gzip.open(self.fOutput, 'w') 
         else:        
-            output = open(self.fOutput, 'w') 
+            output = open(self.fOutput, 'w')
+            seq = ('Chromosome','Region start pos','Region end pos','Gene ID','Gene name','Flag','Strand','Type of region','Number of exon/intron','Total exon/intron','Functional type','Length(nt)', 'Toatal cross-link sites in region',
+                   'Number of pos where cross-links are located', 'Max count in one pos','Count density','Total remove duplicates','Max no.of removed duplicates')
+            output.write(str("\t").join(seq) + "\n")
         
         #Get the information for normalisation of the plots  
         if self.fCompare.endswith(".gz"):
@@ -152,7 +156,11 @@ class bedCLIP:
         if self.fOutput.endswith(".gz"):
             output = gzip.open(self.fOutput, 'w') 
         else:        
-            output = open(self.fOutput, 'w') 
+            output = open(self.fOutput, 'w')
+            seq = ('Chromosome','Region start pos','Region end pos','Gene ID','Gene name','Flag','Strand','Type of region','Number of exon or intron','Total exons or introns',
+                   'Functional type','Length in nt', 'Total cross-link sites in region','Positions where crosslinks are located', 'Max height',
+                   'Density','Total before duplication removal','Max height before duplication removal ')
+            output.write(str("\t").join(seq) + "\n")
         
         #Get the information for normalisation of the plots  
         if self.fCompare.endswith(".gz"):
@@ -288,7 +296,7 @@ class bedCLIP:
                                 check = False
                                 
             if not intergenicCounts == 0:
-                seq = (chrom, '~', '~', '~', '~', strand, 'intergenic', '~', '~', 'intergenic', '~', str(intergenicCounts), '~', '~', '~', '~', '~')
+                seq = (chrom, '~', '~', '~', '~','~', strand, 'intergenic', '~', '~', 'intergenic', '~', str(intergenicCounts), '~', '~', '~', '~', '~')
                 output.write(str("\t").join(seq) + "\n")
                                 
                     
@@ -333,14 +341,14 @@ class bedCLIP:
          
         if len(B) > 0:
               
-            #first exon position in chromosom   
+            #first exon position in chromosome
             b_First = B[0]
-            #last exon postion in chromosom
-            b_Last  = B[-1]
+            #last exon position in chromosome
+            b_Last = B[-1]
               
             bi = 0
                 
-            #foreach cl       
+            #for each cl
             for a in A:
                   
                 check = True
@@ -547,32 +555,32 @@ class bedCLIP:
     Write in output file
     '''
     def writeOut(self, chrom, strand, b, d_count, d_dup, length, output):
-        
+
         name = b[2].split("@")
-        posi = name[3].split("/")
-                            
+        posi = name[4].split("/")
+
         if len(d_count) > 0:
-                  
+
             counts = 0
             for key in d_count:
                 if not d_count[key] == 0:
                     counts += 1
             m_count = max(d_count.keys(), key=(lambda k: d_count[k]))
-                                
+
             dup_counts = 0
             for dup in d_dup:
                 if not d_dup[dup] == 0:
-                    dup_counts += d_dup[dup] 
+                    dup_counts += d_dup[dup]
             m_dup = max(d_dup.keys(), key=(lambda k: d_dup[k]))
-            
-            density = float(counts) / float(length) 
-                                
-            seq = (chrom, str(b[0]+1), str(b[1]+1), name[0], str(b[3]), strand, name[2], posi[0], posi[1], name[1], str(length), str(sum(d_count.values())), str(counts), str(d_count[m_count]), str(density), str(dup_counts), str(d_dup[m_dup]))
+
+            density = float(counts) / float(length)
+
+            seq = (chrom, str(b[0]+1), str(b[1]+1), name[0],name[1], str(b[3]), strand, name[3], posi[0], posi[1], name[2], str(length), str(sum(d_count.values())), str(counts), str(d_count[m_count]), str(density), str(dup_counts), str(d_dup[m_dup]))
             output.write(str("\t").join(seq) + "\n")
         else:
-            seq = (chrom, str(b[0]+1), str(b[1]+1), name[0], str(b[3]), strand, name[2], posi[0], posi[1], name[1], str(length), str(0), str(0), str(0), str(0), str(0), str(0))
+            seq = (chrom, str(b[0]+1), str(b[1]+1), name[0],name[1], str(b[3]), strand, name[3], posi[0], posi[1], name[2], str(length), str(0), str(0), str(0), str(0), str(0), str(0))
             output.write(str("\t").join(seq) + "\n")
-     
+
     #===================================================================================
     
     
