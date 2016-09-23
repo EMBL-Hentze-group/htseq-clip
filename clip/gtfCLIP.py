a1c279fb5e2b4f92cb37b3ff8d58e474ac577b52
nadiaashraf <nadia.ashraf98@gmail.com>
2016-09-23 15:04:30 +0000

    flagging fixed

--- clip/gtfCLIP.py
+++ clip/gtfCLIP.py
@@ -28,10 +28,10 @@ class gtfCLIP:
             self.gtfFile = options.gtf
             
         if hasattr(options, 'input'):
-            self.fInput = options.input
+            self.fInput = options.input[0]
         
         if hasattr(options, 'output'):
-            self.fOutput = options.output  
+            self.fOutput = options.output[0]
             
         if hasattr(options, 'windowSize'):
             self.windowSize = options.windowSize  
@@ -41,10 +41,9 @@ class gtfCLIP:
             
         if hasattr(options, 'type'):
             self.geneType = options.type 
-        
+
         if hasattr (options,'name'):
             self.geneName = options.name
-            
             
     '''
     This method processes through the gtf file and determinates the positions of exons and introns
@@ -80,39 +79,42 @@ class gtfCLIP:
             
             #if gene a new region is found
             #else a new exon is found
-            if feature.type == "gene" or feature.type == "tRNAscan":
+            if feature.type == "gene" or feature.type == "tRNAscan" or "gene" in feature.type:
                 
                 #if genomic array of sets (gas) is none generate a new one
                 #else calculate positions of exon or intron (always starts and ends with exon
                 #between are introns
                 if gas == None:
                     gas = HTSeq.GenomicArrayOfSets('auto', stranded = True)
+
+
                 else:
+
                     #Generate a list for all exons in gene
                     l = list(gas[HTSeq.GenomicInterval(chrom, start, end, strand)].steps())
-                    
+
                     #Sort the list where all exons are in
                     ps.sort(key=lambda tup: tup[0])
                     
                     #calculate number of introns and exons
-                    iLength = int(len(l)/2)
-                    eLength = int(len(l)/2) + 1
+                    # iLength = int(len(l)/2)
+                    # eLength = int(len(l)/2) + 1
                     
-                    exon = True
+                    #exon = True
                          
                     #Depending on if current feature is exon or intron calculate the correct position
                     #and flag the exons
                     for e in l:
-                        if exon:
+                        if e[1]:
                             
                             while len(ps) > 0 and ps[0][0] < e[0].end:
                                 if ps[0][0] == e[0].start and ps[0][1] == e[0].end:
                                     eFlag = 3
-                                elif ps[0][0] == e[0].start and ps[0][1] < e[0].end and eFlag != 0:
+                                elif ps[0][0] == e[0].start and ps[0][1] != e[0].end and eFlag != 0:
                                     eFlag = 2
-                                elif ps[0][0] > e[0].start and ps[0][1] == e[0].end and eFlag != 0:
+                                elif ps[0][0] != e[0].start and ps[0][1] == e[0].end and eFlag != 0:
                                     eFlag = 1
-                                elif  ps[0][0] > e[0].start and ps[0][1] < e[0].end and eFlag != 0:
+                                elif ps[0][0] != e[0].start and ps[0][1] != e[0].end and eFlag != 0:
                                     eFlag = 0
                                 del ps[0]
                             
@@ -128,10 +130,11 @@ class gtfCLIP:
                                 else:
                                     typeFooter[t] += (e[0].end - e[0].start)
                                                    
-                            seq = [e[0].chrom, str(e[0].start), str(e[0].end), name+'@exon@'+str(ec)+'/'+str(eLength), eFlag, e[0].strand, "e"]
+                            #seq = [e[0].chrom, str(e[0].start), str(e[0].end), name+'@exon@'+str(ec)+'/'+str(eLength), eFlag, e[0].strand, "e"]
+                            seq = [e[0].chrom, str(e[0].start), str(e[0].end), name+'@exon@'+str(ec), eFlag, e[0].strand, "e"]
                             fs.append(seq)
                             ec += 1
-                            exon = False
+                            #exon = False
     
                         else:
                             
@@ -141,54 +144,63 @@ class gtfCLIP:
                                     typeFooter["protein_coding_intron"] = (e[0].end - e[0].start)
                                 else:
                                     typeFooter["protein_coding_intron"] += (e[0].end - e[0].start)
-                            else:    
+                            else:
                                 if not typeFooter.has_key(t):
                                     typeFooter[t] = (e[0].end - e[0].start)
                                 else:
                                     typeFooter[t] += (e[0].end - e[0].start)
-                                
-                            seq = [e[0].chrom, str(e[0].start), str(e[0].end), name+'@intron@'+str(ic)+'/'+str(iLength), 1, e[0].strand, "i"]
+
+                            #seq = [e[0].chrom, str(e[0].start), str(e[0].end), name+'@intron@'+str(ic)+'/'+str(iLength), 1, e[0].strand, "i"]
+                            seq = [e[0].chrom, str(e[0].start), str(e[0].end), name+'@intron@'+str(ic), 1, e[0].strand, "i"]
                             fs.append(seq)
                             ic += 1
-                            exon = True
-                            
+
+                    eCount = ec-1
+                    iCount = ic-1
+
                     #Flagging of introns and writing out in output file
                     #else there is only one exon
                     if not len(fs) == 1:
-                            
-                        #Initialisation of Flags
-                        p = 0
-                        eFlag1 = fs[p][4]
-                        p = p + 2
-                        eFlag2 = fs[p][4]
-                             
-                        for line in fs:
-                            if line[6] == "e":
-                                seq = (line[0], line[1], line[2], line[3], str(line[4]), line[5], "\n")
-                                output.write(str('\t').join(seq))
-                            elif line[6] == "i":
-                                
-                                #Simple binary operations to calculate the flag for the introns
-                                #The intron flag is calculated by using the left exon flag and the 
-                                #right exon flag by using the bits of the binary coded flag
-                                #for further information how they are calculated look up the
-                                #documentation
-                                line[4] = ((eFlag1 & 1) << 1) | (eFlag2 >> 1)    
-                                
-                                seq = (line[0], line[1], line[2], line[3], str(line[4]), line[5], "\n")
-                                output.write(str('\t').join(seq))
-                 
-                                eFlag1 = eFlag2
-                                p = p + 2
-                                if not p > len(fs):
+                            # Initialisation of Flags
+
+                        if len(fs) == 2:
+                            for line in fs:
+                                if line[6] == "e":
+                                    seq = (line[0], line[1], line[2], line[3]+'/'+str(eCount), str(line[4]), line[5], "\n")
+                                    output.write(str('\t').join(seq))
+                                elif line[6] == "i":
+                                    seq = (line[0], line[1], line[2], line[3]+'/'+str(iCount), str(line[4]), line[5], "\n")
+                                    output.write(str('\t').join(seq))
+                        else:
+                            for val in range(len(fs)):
+                                if fs[val][6] == 'i':
+
+                                    p = val-1
+                                    eFlag1 = fs[p][4]
+                                    p = p + 1
                                     eFlag2 = fs[p][4]
+                                    #Simple binary operations to calculate the flag for the introns
+                                    #The intron flag is calculated by using the left exon flag and the
+                                    #right exon flag by using the bits of the binary coded flag
+                                    # for further information how they are calculated look up the
+                                    # documentation
+                                    fs[val][4] = ((eFlag1 & 1) << 1) | (eFlag2 >> 1)
+
+                                    seq = (fs[val][0], fs[val][1], fs[val][2], fs[val][3]+'/'+str(iCount), str(fs[val][4]), fs[val][5], "\n")
+                                    output.write(str('\t').join(seq))
+
+
+                                elif fs[val][6] == "e":
+                                    seq = (fs[val][0], fs[val][1], fs[val][2], fs[val][3]+'/'+str(eCount), str(fs[val][4]), fs[val][5], "\n")
+                                    output.write(str('\t').join(seq))
+
                     else:
                         #if gene type is is something else, you have to flag it with 3 because there only exists one isoform
                         if fs[0][4] == None:
                             fs[0][4] = 3
-                        seq = (fs[0][0], fs[0][1], fs[0][2], fs[0][3], str(fs[0][4]), fs[0][5], "\n")
-                        output.write(str('\t').join(seq))                                 
-                                
+                        seq = (fs[0][0], fs[0][1], fs[0][2], fs[0][3]+'/'+str(eCount), str(fs[0][4]), fs[0][5], "\n")
+                        output.write(str('\t').join(seq))
+
                             
                     gas = HTSeq.GenomicArrayOfSets('auto', stranded = True)
                 
@@ -208,7 +220,7 @@ class gtfCLIP:
                     error = "Wrong gene type: "+self.geneType+". Check your annotation file!!"
                     raise KeyError(error)
                          
-                if self.geneName:
+                if len(self.geneName) != 0:
                     if feature.attr.has_key(self.geneName):
                         gene_name = str(feature.attr[str(self.geneName)])
                     else:
@@ -219,6 +231,7 @@ class gtfCLIP:
                 else:
                     name = name+'@'+attribute
                 t = attribute
+
                 start = feature.iv.start
                 end = feature.iv.end
                 ec = 1
@@ -230,8 +243,12 @@ class gtfCLIP:
                 giv = HTSeq.GenomicInterval(feature.iv.chrom, feature.iv.start, feature.iv.end, feature.iv.strand)
                 ps.append((feature.iv.start, feature.iv.end))
                 s = 'exon'
-                gas[giv] += s    
-        
+                gas[giv] += s
+            if feature.type == "tRNAscan":
+                giv = HTSeq.GenomicInterval(feature.iv.chrom, feature.iv.start, feature.iv.end, feature.iv.strand)
+                ps.append((feature.iv.start, feature.iv.end))
+                s = 'exon'
+                gas[giv] += s
         #Write out information for normalization of plots
         chromFooter = OrderedDict(sorted(chromFooter.items(), key=lambda x: (-x[1], x[0])))  
         
@@ -242,8 +259,8 @@ class gtfCLIP:
         
         for k in typeFooter:
             output.write("track type "+str(k)+" "+str(typeFooter[k])+"\n")
-        
-        if self.geneName:
+
+        if len(self.geneName) != 0:
             output.write("track"+" "+"gene_name"+"\n")
         else:
             output.write("track"+"\n")
