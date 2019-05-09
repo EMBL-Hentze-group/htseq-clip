044686d3c883e14e9dd461b71a97c3023a5817a9
sahadeva <sudeep.sahadevan@embl.de>
2019-05-09 08:59:59 +0200

    changes to help descriptions

--- clip/clip.py
+++ clip/clip.py
@@ -80,7 +80,7 @@ def main():
     description = '''
     {0}:  A flexible toolset for the analysis of iCLIP and eCLIP sequencing data
 
-    The functions (as positional arguments) should be one of:
+    The function (as a positional argument) should be one of:
 
     [Annotation]
         annotation              flattens an annotation gtf file
@@ -93,6 +93,9 @@ def main():
         count                   count sites in annotation
         countSlidingWindows     count sites in sliding windows
         feature                 count sites in repeated regions
+    
+    [Transformation]
+        slidingWindowsToDEXSeq  transform sliding window counts to DEXSeq format
         
     [Distances]
         junction                calculates distances to junctions
@@ -100,10 +103,7 @@ def main():
 
     [Visualisation] 
         plot                    visualisation 
-        
-    [Transformation]
-        slidingWindowsToDEXSeq  transform sliding window counts to DEXSeq format
-        
+           
     [General help]
         -h, --help               help
     '''.format(prog)
@@ -114,19 +114,19 @@ def main():
 
     ''' ____________________ [Annotation] ___________________  '''
     # annotation
-    ahelp = 'annotation: flattens (BED format) the given annotation file (GFF format)'
+    ahelp = 'annotation: flattens (to BED format) the given annotation file (in GFF format)'
     annotation = subps.add_parser('annotation',description=ahelp) # help='flatten annotation',
     annotation.add_argument('-g','--gff',metavar='annotation',dest='gff',help='GFF3 file for annotation processing, supports gzipped (.gz) files',required=True)
-    annotation.add_argument('-o','--output',metavar = 'output file',dest='output',help='output file (.bed/.bed.gz, default: print to console)',default=None,type=str)
+    annotation.add_argument('-o','--output',metavar = 'output file',dest='output',help='output file (.bed[.gz], default: print to console)',default=None,type=str)
     annotation.add_argument('-u','--geneid',metavar='gene id',dest='id',help='Gene id attribute in GFF file (default: gene_id for gencode gff files)',default='gene_id',type=str)
     annotation.add_argument('-n','--genename',metavar='gene name',dest='name',help='Gene name attribute in GFF file (default: gene_name for gencode gff files)',default='gene_name',type=str)
     annotation.add_argument('-t','--genetype',metavar='gene type',dest='type',help='Gene type attribute in GFF file (default: gene_type for gencode gff files)',default='gene_type',type=str)
     annotation.add_argument('--unsorted',dest='unsorted',help='use this flag if the GFF file is unsorted',action='store_true')
     # createSlidingWindows
     cshelp = 'createSlidingWindows: creates sliding windows out of the flattened annotation file'
-    createSlidingWindows = subps.add_parser('createSlidingWindows',description=cshelp) # help='create sliding windows',
+    createSlidingWindows = subps.add_parser('createSlidingWindows',description=cshelp, formatter_class=argparse.RawTextHelpFormatter) # help='create sliding windows',
     createSlidingWindows.add_argument('-i','--input',metavar='input file',dest='input',help='input flattend annotation file, see "{} annotation -h"'.format(prog),required=True)
-    createSlidingWindows.add_argument('-o','--output',metavar = 'output file',dest='output',help='output annotation sliding windows file (.bed/.bed.gz, default: print to console)',default=None,type=str)
+    createSlidingWindows.add_argument('-o','--output',metavar = 'output file',dest='output',help='output annotation sliding windows file (.bed[.gz], default: print to console)',default=None,type=str)
     createSlidingWindows.add_argument('-w','--windowSize',metavar = 'window size',dest='windowSize',help='window size for sliding window (default: 50)',default=50,type=int)
     createSlidingWindows.add_argument('-s','--windowStep',metavar = 'step size',dest='windowStep',help='window step size for sliding window (default: 20)',default=20,type=int)
     ''' ____________________ [Extraction] ___________________ '''
@@ -137,9 +137,9 @@ def main():
     extract = subps.add_parser('extract',description=ehelp,formatter_class=argparse.RawTextHelpFormatter) #,help='extract crosslinks'
     extract.add_argument('-i','--input',metavar='input file',dest='input',help='input file (.bam)',required=True)
     extract.add_argument('-o','--output',metavar = 'output file',dest='output',help='output file (.bed, default: print to console)',default=None,type=str)
-    extract.add_argument('-e','--mate',metavar='mate info',dest='mate',help='select which read to extract the crosslink sites from. Allowed choices: {}'.format(', '.join([str(i) for i in mates])),type=int,choices=mates,required=True) # make it required ?
+    extract.add_argument('-e','--mate',metavar='mate info',dest='mate',help='for paired end sequencing, select the read/mate to extract the crosslink sites from.\n Allowed inputs: {}'.format(', '.join([str(i) for i in mates])),type=int,choices=mates,required=True) # make it required ?
     extract.add_argument('-s','--site',dest='choice',
-        help='Crosslink site choices: Allowed choices: {0}\n s: start site \n i: insertion site \n d: deletion site \n m: middle site \n e: end site (default: e).'.format(', '.join(echoices)),choices=echoices,default='e')
+        help='Crosslink site choices, must be one of: {0}\n s: start site \n i: insertion site \n d: deletion site \n m: middle site \n e: end site (default: e).'.format(', '.join(echoices)),choices=echoices,default='e')
     extract.add_argument('-g','--offset',metavar='offset length',dest='offset',help='Number of nucleotides to offset for crosslink sites (default: 0)',type=int,default=0)
     extract.add_argument('--ignore',dest='ignore',help='flag to ignore crosslink sites outside of genome',action='store_true')
     extract.add_argument('-q','--minAlignmentQuality',metavar = 'min. alignment quality',dest='minAlignmentQuality',help='minimum alignment quality (default: 10)',type=int,default=10)
@@ -156,10 +156,10 @@ def main():
     count.add_argument('-o','--output',metavar = 'output file',dest='output',help='output count file (.txt[.gz], default: print to console)',default=None,type=str)
     count.add_argument('-a','--ann',metavar = 'annotation',dest='compare',help='flattened annotation file (.bed[.gz]), see "{} annotation -h"'.format(prog),required=True)
     count.add_argument('-c','--count',dest='choice',
-        help='Crosslink site count choices: Allowed choices: {0} \n o: only include features (exons/introns) with crosslink site \n a: include all features, even with 0 crosslink sites. (default: o).'.format(', '.join(cchoices)),choices=cchoices,default='o')
+        help='Crosslink site count choices, must be one of: {0} \n o: only include features (exons/introns) with crosslink site \n a: include all features, even with 0 crosslink sites. (default: o).'.format(', '.join(cchoices)),choices=cchoices,default='o')
     # countSlidingWindows
     cswhelp = 'countSlidingWindows: counts the number of crosslink/deletion/insertion sites in a certain sliding window'
-    countSlidingWindows = subps.add_parser('countSlidingWindows', description=cswhelp) # help='count sliding windows',
+    countSlidingWindows = subps.add_parser('countSlidingWindows', description=cswhelp, formatter_class=argparse.RawTextHelpFormatter) # help='count sliding windows',
     countSlidingWindows.add_argument('-i','--input',metavar='input bed',help='extracted crosslink, insertion or deletion sites (.bed[.gz]), see "{} extract -h"'.format(prog),required=True)
     countSlidingWindows.add_argument('-o','--output',metavar = 'output file',dest='output',help='output count file (.txt[.gz], default: print to console)',default=None,type=str)
     countSlidingWindows.add_argument('-s','--sw',metavar = 'sliding window',dest='compare',help='sliding window annotation file (.bed[.gz]), see "{} createSlidingWindows -h"'.format(prog),required=True)
@@ -167,29 +167,27 @@ def main():
     ''' ____________________ [Transformation] ___________________ '''
     # slidingWindowsToDEXSeq
     swhelp = 'slidingWindowsToDEXSeq: transforms the sliding window counts into DEXSeq format'
-    slidingWindowsToDEXSeq = subps.add_parser('slidingWindowsToDEXSeq',description=swhelp) # help='to DEXSeq format',
+    slidingWindowsToDEXSeq = subps.add_parser('slidingWindowsToDEXSeq',description=swhelp, formatter_class=argparse.RawTextHelpFormatter) # help='to DEXSeq format',
     slidingWindowsToDEXSeq.add_argument('-i','--input',metavar='input counts',help='sliding window count data file [.gz], see "{} countSlidingWindows -h"'.format(prog),required=True)
     slidingWindowsToDEXSeq.add_argument('-o','--output',metavar = 'output file',dest='output',help='output DEXSeq formatted count (.txt[.gz], default: print to console)',default=None,type=str)
-    # distance
-    dhelp = 'distance: calculates the nearest crosslink site to a region/feature'
-    distance = subps.add_parser('distance',description=dhelp) # help='nearest crosslink site',
-    # feature ?
-    fhelp = 'feature:  counts the number of crosslink/deletion/insertion sites'
-    feature = subps.add_parser('feature',description=fhelp) # help='count crosslinks',
+    ''' ____________________ [Distances] ___________________ '''
     # junction 
     jhelp = 'junction: calculates the distance from crosslink/deletion/insertion sites to the junction'
     junction = subps.add_parser('junction',description=jhelp) # help='crosslink junctions',
     
+    # # distance
+    # dhelp = 'distance: calculates the nearest crosslink site to a region/feature'
+    # distance = subps.add_parser('distance',description=dhelp) # help='nearest crosslink site',
+    # # feature ?
+    # fhelp = 'feature:  counts the number of crosslink/deletion/insertion sites'
+    # feature = subps.add_parser('feature',description=fhelp) # help='count crosslinks',
     
-    
-    # plot
-    phelp  = 'plot: plots htseq-clip analysis results'
-    plot = subps.add_parser('plot',description=phelp) # help = 'plot results',
-    # heatmap
-    hhelp = 'heatmap: plots the heatmap results'
-    heatmap = subps.add_parser('heatmap',description=hhelp) # help='plot heatmap',
-    
-
+    # # plot
+    # phelp  = 'plot: plots htseq-clip analysis results'
+    # plot = subps.add_parser('plot',description=phelp) # help = 'plot results',
+    # # heatmap
+    # hhelp = 'heatmap: plots the heatmap results'
+    # heatmap = subps.add_parser('heatmap',description=hhelp) # help='plot heatmap',
     # Now read in arguments and process
     try:
         args = parser.parse_args()
