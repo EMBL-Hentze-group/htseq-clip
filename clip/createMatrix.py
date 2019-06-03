ddd284a3c2569bee743f8c8c0ce982bd4ec85a9c
sahadeva <sudeep.sahadevan@embl.de>
2019-06-03 09:44:18 +0200

    add gzip support

---
+++ clip/createMatrix.py
@@ -0,0 +1,166 @@
+__author__ = 'Tom'
+
+import os
+import re
+import gzip
+import sys
+import argparse
+
+"""
+Class to convert count files into count matrixes.
+"""
+class MatrixConverter:
+
+    def __init__(self, inputDir, inputPrefix, inputPostfix, outputFilename):
+        # input files
+        self.inputFilenames  = self._dir_filter(inputDir, prefix = inputPrefix, postfix = inputPostfix)
+        # input dir
+        self.inputDir = inputDir
+
+        # output file
+        self.outputFilename  = outputFilename
+        
+        # dict to store the counts
+        self.countDict = {}
+        
+        # dict to store all samples
+        self.allSamples = set()
+        
+        # sample names are stored here for consistent output
+        self.samplenamesList = None
+        
+    """
+    Helper function
+    check if a filename matches a prefix and postfix
+    """
+    def _file_filter(self, filename, prefix = "", postfix = ""):
+        if not filename:
+            return False
+        filename = filename.strip()
+        return(filename.startswith(prefix) and filename.endswith(postfix))
+    
+    
+    """
+    Helper function
+    filter filenames in directory according to prefix and postfix
+    """
+    def _dir_filter(self, dirname, prefix = "", postfix = ""):   
+        return [filename for filename in os.listdir(dirname) if self._file_filter(filename, prefix, postfix)]
+    
+    """
+    read in all samples
+    values will be stored in 
+    """
+    def read_samples(self):
+    	# for each file name
+        for file in self.inputFilenames:
+            samplename = file.split(".")[0]
+            self.allSamples.add(samplename)
+            # open file and read in the content and store the results 
+            with self._file_reader(os.path.join(self.inputDir, file)) as f:
+                for linecount, line in enumerate(f):
+                    if(linecount > 0):
+                        linesplit = line.strip().split("\t")
+                        try:
+                            self.countDict[ linesplit[0] ][ samplename ] = linesplit[3]
+                        except KeyError:
+                            self.countDict[ linesplit[0] ] = { samplename : linesplit[3] }
+        self._init_samplenames_list()
+    
+    def _file_reader(self,fn):
+        '''
+        Helper function, return the correct file reade object based on file suffix
+        Argument:
+         fn: file name as string
+        '''
+        if re.match(r'.*\.gz.*',fn,re.IGNORECASE):
+            return gzip.open(fn,'rt')
+        else:
+            return open(fn,'r')
+
+
+    """
+    Helper function
+    getter for sample names
+    """
+    def _init_samplenames_list(self):
+        self.samplenamesList = sorted(self.allSamples)
+    
+    """
+    Helper function
+    getter for header
+    """
+    def _get_header(self):
+        return("\t".join(["ID"] + self.samplenamesList))
+    
+    
+    """
+    write matrix to output file
+    """
+    def write_matrix(self):
+    	# open output file
+        with open(self.outputFilename, 'w') as out:
+            # write header
+            out.write(self._get_header() + "\n")
+            # write rows
+            for uid, sample_count_dict in self.countDict.items():
+                outList = [uid]
+                # write column
+                for sample_name in self.samplenamesList:
+                    try:
+                        outList.append(sample_count_dict[sample_name])  
+                    except KeyError:
+                        outList.append('0')
+                out.write('\t'.join(outList) + "\n")
+
+
+parser = argparse.ArgumentParser(description = 'Creating a count matrix from htseq-clip count files.')
+
+
+parser.add_argument('--dir',
+                    action   = 'store',
+                    dest     = 'dir',
+                    required = True, 
+                    help     = 'directory of count files')
+                    
+parser.add_argument('--prefix',
+                    action   = 'store',
+                    dest     = 'prefix',
+                    default  = '',
+                    help     = 'prefix of count files')
+
+parser.add_argument('--postfix',
+                    action   = 'store',
+                    dest     = 'postfix',
+                    required = True,
+                    help     = 'postfix of count files')
+                    
+parser.add_argument('--output',
+                    action   = 'store',
+                    dest     = 'output',
+                    required = True, 
+                    help     = 'destination of output count matrix')
+
+args = parser.parse_args()
+
+
+#print args.dir
+#print args.prefix
+#print args.postfix
+#print args.output
+
+
+m1 = MatrixConverter(args.dir, args.prefix, args.postfix, args.output)
+##print("Reading in window counts")
+
+m1.read_samples()
+#print("Writing window counts") 
+
+
+#print m1.output_filename
+#print m1.count_dict
+#print m1.allsamples
+#print m1.input_dir
+#print m1.samplenames_list
+
+m1.write_matrix()
