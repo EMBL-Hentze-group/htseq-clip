698d0771a11890a27e512b5900a685c45684329f
sahadeva <sudeep.sahadevan@embl.de>
2020-01-22 17:01:21 +0100

    start with countCLIP_test.py

---
+++ tests/countCLIP_test.py
@@ -0,0 +1,9 @@
+import unittest
+from argparse import Namespace
+from clip.countCLIP import countCLIP
+
+class TestcountCLIP(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/gffCLIP_test.py
+++ tests/gffCLIP_test.py
@@ -1,4 +1,3 @@
-import os
 import unittest
 from argparse import Namespace
 
