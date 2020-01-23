4443fa3fced48916a3db40367fbb5d89c4604d5a
sahadeva <sudeep.sahadevan@embl.de>
2020-01-23 10:34:16 +0100

    test_annotation

--- tests/countCLIP_test.py
+++ tests/countCLIP_test.py
@@ -3,7 +3,21 @@ from argparse import Namespace
 from clip.countCLIP import countCLIP
 
 class TestcountCLIP(unittest.TestCase):
-    pass
+    '''
+    unit tests for module countCLIP
+    '''
+    def test_annotation(self):
+        options = Namespace(annotation='tests/testcountCLIP/W50S20.bed',output='tests/testcountCLIP/checkcountCLIP/test_annotation.txt')
+        countC = countCLIP(options)
+        countC.annotationToIDs()
+        annSet, expSet = set(),set()
+        with open('tests/testcountCLIP/checkcountCLIP/test_annotation.txt','r') as ann:
+            for l in ann:
+                annSet.add(l)
+        with open('tests/UnitTestOutput/testcountCLIP/test_annotation.txt','r') as eann:
+            for l in eann:
+                expSet.add(l)
+        self.assertSetEqual(annSet,expSet)
 
 if __name__ == '__main__':
     unittest.main()
